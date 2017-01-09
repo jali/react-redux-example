06bc57df48d774e0de926b5d45297c0c6f4fd1c9
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2017-01-09 15:00:10 +0000

    correct stage

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-3'],
+          presets: ['react', 'es2015', 'stage-0'],
           plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
         }
       }
