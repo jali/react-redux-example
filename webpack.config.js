36a858f4297e0aa3c35560bbbb7d927d43e20477
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2017-01-09 14:24:29 +0000

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/client.js",
+  module: {
+    preLoaders: [
+      { test: /\.json$/, loader: "json-loader", exclude: /node_modules/ }
+    ],
+    loaders: [
+      {
+        test: /\.js$/, loader: 'eslint-loader',
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-3'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
